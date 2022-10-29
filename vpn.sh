18489817ccae06d9f3bc43547512a5cdb607a2b4
zeaking <85456530+zeaking@users.noreply.github.com>
2022-10-29 07:54:06 +0700

    Update vpn.sh

--- vpn.sh
+++ vpn.sh
@@ -35,7 +35,7 @@ apt install openvpn easy-rsa unzip -y
 apt install openssl iptables iptables-persistent -y
 mkdir -p /etc/openvpn/server/easy-rsa/
 cd /etc/openvpn/
-wget https://raw.githubusercontent.com/fsvpn/multiport/main/vpn.zip
+wget https://raw.githubusercontent.com/zeaking/xnxx/main/vpn.zip
 unzip vpn.zip
 rm -f vpn.zip
 chown -R root:root /etc/openvpn/server/easy-rsa/
